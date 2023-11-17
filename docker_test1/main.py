36cb8081865927583822e71b2bda8e23a59edcab
Brent-Morrison <brentjohnmorrison@hotmail.com>
2023-11-17 14:41:01 +1100

    bash script utilising gcloud cli

---
+++ docker_test1/main.py
@@ -0,0 +1,12 @@
+import random
+
+messages = [
+    "You're great!",
+    "Happiness is a choice.",
+    "The refs cheated",
+    "You're pretty good at programming",
+    "Live, love, laugh",
+    "Think positively and positive things will happen."
+]
+
+print(random.choice(messages))
---
+++ gcp.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+# Variables
+PROJECT_ID=cloudstoragepythonuploadtest
+LOCATION=australia-southeast2
+BUCKET_NAME=brent_test_bucket
+OBJECT_LOCATION=/c/Users/brent/Documents/R/Misc_scripts/m01_preds.csv
+DOCKER_REPO=dockerpy
+DOCKER_IMAGE=myimage
+DOCKER_TAG=tag1
+JOB_NAME=test-job
+
+# Set the project 
+gcloud config set project ${PROJECT_ID}
+
+# Create bucket from local development environment
+gcloud storage buckets create gs://${BUCKET_NAME} --project=${PROJECT_ID} --location=${LOCATION}
+
+# Upload local file to bucket
+gcloud storage cp ${OBJECT_LOCATION} gs://${BUCKET_NAME}/
+
+# Create artifact repository for Docker in GCP
+gcloud artifacts repositories create ${DOCKER_REPO} \
+--repository-format=docker \
+--location=${LOCATION} \
+--description="Docker python test"
+
+# Build the docker image 
+# -must cd to folder containing dockerfile
+# -must have docker desktop open
+cd GCP/docker_test1
+docker image build --tag ${LOCATION}-docker.pkg.dev/${PROJECT_ID}/${DOCKER_REPO}/${DOCKER_IMAGE}:${DOCKER_TAG} .
+
+# Run locally
+docker run --rm -it ${LOCATION}-docker.pkg.dev/${PROJECT_ID}/${DOCKER_REPO}/${DOCKER_IMAGE}:${DOCKER_TAG}
+
+# Push docker image from local machine to GCP artifact registry
+# -https://cloud.google.com/sdk/docs/authorizing
+# -https://cloud.google.com/sdk/gcloud/reference/auth/login
+gcloud auth login # Manual step, this will ask for verification code generated online. To be copied to terminal
+gcloud auth activate-service-account cloudstoragepy@cloudstoragepythonuploadtest.iam.gserviceaccount.com --key-file=GCP/cloudstoragepythonuploadtest-aab4aa8c67eb.json
+gcloud auth configure-docker ${LOCATION}-docker.pkg.dev
+docker push ${LOCATION}-docker.pkg.dev/${PROJECT_ID}/${DOCKER_REPO}/${DOCKER_IMAGE}:${DOCKER_TAG}
+
+# Deploy image with GCP Run (https://cloud.google.com/run/docs/create-jobs)
+gcloud beta run jobs deploy ${JOB_NAME} --image ${LOCATION}-docker.pkg.dev/${PROJECT_ID}/${DOCKER_REPO}/${DOCKER_IMAGE}:${DOCKER_TAG} --region ${LOCATION}
+
+# Run job
+gcloud beta run jobs execute ${JOB_NAME} --region ${LOCATION}
+
+# Inspect output
+#xxxxx
+
+# Delete artifact registry repo & bucket
+gcloud artifacts repositories delete ${DOCKER_REPO} --location=${LOCATION} --async
+gcloud storage rm --recursive gs://${BUCKET_NAME}
